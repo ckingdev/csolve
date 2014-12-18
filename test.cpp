b2a7fbd8b4a8b64d4d1ed7ad2fa932b25119fe0c
Cameron Palone <mcameronpalone@gmail.com>
2014-12-18 14:51:04 -0700

    Continuing to build out test suite.

--- test.cpp
+++ test.cpp
@@ -2,7 +2,8 @@
 #include "Puzzle.hpp"
 #include <vector>
 
-TEST(MovesTest, FourMovesIsIdentity) {
+TEST(MovesTest, FourMovesIsIdentity)
+{
 	Puzzle p = get_full();
 	std::vector<Move> moves = init_three_moves();
 	for (int i = 0; i < 6; i++) {
@@ -22,4 +23,16 @@ TEST(MovesTest, MovesLen18)
 {
 	std::vector<Move> moves = init_three_moves();
 	EXPECT_EQ(18, moves.size());
+}
+
+TEST(PuzzleTest, IdentityIsSolved)
+{
+	auto p = get_full();
+	EXPECT_TRUE(p.solved());
+}
+
+TEST(PuzzleTest, CrossIsSolved)
+{
+	auto p = get_cross();
+	EXPECT_TRUE(p.solved());
 }
