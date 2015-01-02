c2046a2f5c90e192c2d9af37a4d1165cca6f75d6
Cameron Palone <mcameronpalone@gmail.com>
2015-01-02 11:40:17 -0700

    Preparing for merge into master, oops.

--- main.cpp
+++ main.cpp
@@ -9,10 +9,10 @@
 int main() {
     auto p = get_4_corners();
     auto base_moves = get_2x2_h3gen_turns();
-    auto pt = gen_pruning_table(p, 6, base_moves);
-    // std::ofstream outfile;
-    // outfile.open("2x2_4corner.prune");
-    // cereal::BinaryOutputArchive oarchive(outfile);
-    // oarchive(pt);
+    auto pt = gen_pruning_table(p, 11, base_moves);
+    std::ofstream outfile;
+    outfile.open("2x2_4corner.prune");
+    cereal::BinaryOutputArchive oarchive(outfile);
+    oarchive(pt);
     return 0;
 }
