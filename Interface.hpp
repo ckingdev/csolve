f201ac961f5ad69a66815508706d4c51a6f76fb3
Cameron Palone <mcameronpalone@gmail.com>
2015-01-02 15:02:18 -0700

    Wrote a function to save a PruningTable to file.

---
+++ Interface.hpp
@@ -0,0 +1,12 @@
+#include <string>
+#include "Puzzle.hpp"
+#include <fstream>
+
+template <typename puzzle>
+void save_pruning_table(std::string f_name, typename puzzle::PruningTable &pt) {
+        std::ofstream outfile;
+        outfile.open(f_name);
+        cereal::BinaryOutputArchive oarchive(outfile);
+        oarchive(pt);
+}
+
--- main.cpp
+++ main.cpp
@@ -1,18 +1,17 @@
 #include "Puzzle.hpp"
 #include "PruningTable.hpp"
 #include "cereal/archives/binary.hpp"
+#include "Interface.hpp"
 
 #include <cstdlib>
 #include <cstdio>
+#include <string>
 #include <fstream>
 
 int main() {
     auto p = get_4_corners();
     auto base_moves = get_2x2_h3gen_turns();
-    auto pt = gen_pruning_table(p, 11, base_moves);
-    std::ofstream outfile;
-    outfile.open("2x2_4corner.prune");
-    cereal::BinaryOutputArchive oarchive(outfile);
-    oarchive(pt);
+    decltype(p)::PruningTable pt = gen_pruning_table(p, 8, base_moves);
+    save_pruning_table<decltype(p)>("test.prune", pt);
     return 0;
 }
