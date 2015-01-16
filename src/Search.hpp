60248b3b495b2e81b4bdfbe28ca03106853c7b45
M. Cameron Palone <mcameronpalone@gmail.com>
2015-01-16 14:29:45 -0700

    Added some code in main.cpp to benchmark solving random 2x2 cubes.

--- src/Search.hpp
+++ src/Search.hpp
@@ -11,6 +11,14 @@ struct SearchNode {
 	std::vector<int> moves_taken;
 };
 
+int get_axis(int move) {
+	if (move < 3) {
+		return move;
+	} else {
+		return (move - 1) / 3;
+	}
+}
+
 template<typename puzzle, int d>
 std::vector<int> A_star_single(puzzle root, typename puzzle::PruningTable &pt, 
                                int pq_size, std::vector<Move> move_set) {
@@ -20,7 +28,17 @@ std::vector<int> A_star_single(puzzle root, typename puzzle::PruningTable &pt,
 	pq.insert(root_node, 0);
 	while (!pq.is_empty()) {
 		auto state = pq.pop();
+		// int prev_axis;
+		// if (state.moves_taken.size() > 0) {
+		// 	int prev_move = state.moves_taken.back();
+		// 	prev_axis = get_axis(prev_move);
+		// } else {
+		// 	prev_axis = -1;
+		// }
 		for (int i = 0; i < n_moves; i++) {
+			// if (get_axis(i) == prev_axis) {
+			// 	continue;
+			// }
 			std::vector<int> new_moves_taken = state.moves_taken;
 			new_moves_taken.push_back(i);
 			puzzle new_state = state.state.apply(move_set[i]);
--- src/main.cpp
+++ src/main.cpp
@@ -11,35 +11,37 @@
 #include <fstream>
 #include <iostream>
 #include <vector>
+#include <stdlib.h>
+#include <boost/progress.hpp>
+
+std::vector<int> get_rand_seq(int max, int len) {
+    std::vector<int> seq;
+    for (int i = 0; i < len; i++) {
+        seq.push_back(rand() % max);
+    }
+    return seq;
+}
+
+
+Puzzle<0,8> get_random_2x2_scramble(int n_moves, int len_scramble, std::vector<Move> move_set) {
+    Puzzle<0,8> tmp = get_corners();
+    std::vector<int> scramble = get_rand_seq(n_moves, len_scramble);
+    for (auto &i : scramble) {
+        tmp = tmp.apply(move_set[i]);
+    }
+    return tmp;
+}
 
 int main() {
-    // auto p = get_corners();
-    // auto base_moves = get_2x2_h3gen_turns();
-    // decltype(p)::PruningTable pt = gen_pruning_table(p, 11, base_moves);
-    // std::cout << pt.size() << std::endl;
-    // save_pruning_table<decltype(p)>("test.prune", pt);
-    // std::string s = "R U F R' F2";
-    // auto moves = parse_move_string(s, get_2x2_3gen_parse_table());
-    // for (auto &i : moves) {
-    //     std::cout << i << std::endl;
-    // }
-    // 
-    // int n = 100;
-    // MinHeap<int, 50> mh(n);
-    // for (int i = 0; i < n; i++) {
-    //     mh.insert(n-i, n-i);
-    // }
-    // for (int i = 0; i < n; i++) {
-    //     std::cout<< mh.pop() << " ";
-    // }
-    // mh.print();
-    // return 0;
-    // 
     auto p = get_corners();
     auto base_moves = get_2x2_h3gen_turns();
     decltype(p)::PruningTable pt = bfs_pruning_table(p, 11, base_moves);
-    p = p.apply(base_moves[0]);
-    p = p.apply(base_moves[1]);
-    std::vector<int> sol = A_star_single<Puzzle<0, 8>, 2>(p, pt, 100, base_moves);
-    std::cout << sol[0] << std::endl;
+    std::cout << "Pruning table generated.\n";
+    boost::progress_timer timer;
+    for (int i = 0; i < 100000; i++) {
+        auto to_solve = get_random_2x2_scramble(9, 25, base_moves); 
+        std::vector<int> sol = A_star_single<Puzzle<0, 8>, 9>(to_solve, pt, 1000, base_moves);
+        sol.clear();
+    }
+    std::cout << "\n";
 }
